57aa6bd61a7cebe36feccfd49933518db135ef44
Demian Romanenko <romanenko@niuitmo.ru>
2020-08-25 16:56:39 +0300

    + quine

---
+++ term4/quine/quine.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let love = "I love discrete math";
+    print!("fn main() {{\n    let love = \"I love discrete math\";\n    print!({0:#?}, {0:#?});\n}}", "fn main() {{\n    let love = \"I love discrete math\";\n    print!({0:#?}, {0:#?});\n}}");
+}
